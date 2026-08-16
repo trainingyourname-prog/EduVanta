6eb09f84c48c74d74ea8cfb8e323fc2b752c5d6f
trainingyourname-prog <training.yourname@gmail.com>
2026-08-16 07:10:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+
+    const preloader = document.getElementById("preloader");
+
+    if (preloader) {
+
+        preloader.style.display = "none";
+
+    }
+
+});
